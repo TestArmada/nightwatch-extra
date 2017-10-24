917e4aa3c12cc1cb9dfb3979af2f89c1da2a8086
Marro Ung <marroung@gmail.com>
2017-10-23 17:12:31 -0700

    additional updates

--- src/commands/mobile/scrollDownToElement.js
+++ src/commands/mobile/scrollDownToElement.js
@@ -1,6 +1,8 @@
 import util from "util";
 import BaseCommand from "../../base-mobile-command";
 
+const MAX_ATTEMPTS = 10;
+
 const ScrollDownToElement = function (nightwatch = null) {
   BaseCommand.call(this, nightwatch);
   this.cmd = "scrolldowntoelement";
@@ -8,18 +10,35 @@ const ScrollDownToElement = function (nightwatch = null) {
 
 util.inherits(ScrollDownToElement, BaseCommand);
 
-ScrollDownToElement.prototype.command = function (xStart, yStart, xEnd, yEnd, elementLocateStrategy, elementSelector, maxAttempts) {
+ScrollDownToElement.prototype.command = function (xStart, yStart, elementLocateStrategy, elementSelector, cb) {
   const self = this;
+  this.selector = elementSelector;
+  this.using = elementLocateStrategy;
+  this.cb = cb;
 
-  const scrollToFindElement = function (maxAttempts, attempts) {
+  this.failureMessage = `Selector '${this.using}:${this.selector}' `
+    + `was not visible after scrolling ${MAX_ATTEMPTS} times`;
 
-    if (attempts < maxAttempts) {
+  const scrollToFindElement = function (attempts) {
+
+    if (attempts < MAX_ATTEMPTS) {
       self.client.api.getMobileElConditional(elementLocateStrategy,
         elementSelector, 1000,
           (result) => {
             if (!result) {
-              self.client.api.swipeScreenTo(xStart, yStart, xEnd, yEnd);
-              return scrollToFindElement(maxAttempts, attempts + 1);
+              if (self.client.api.capabilities.platformName === 'Android') {
+                let yEnd = 1;
+                if (yStart > 200) {
+                  yEnd = yStart - 200;
+                }
+                self.client.api.swipeScreenTo(xStart, yStart, 10, yEnd);
+              }
+              else if (self.client.api.capabilities.platformName === 'iOS') {
+                self.client.api.swipeScreenTo(xStart, yStart, 0, -200);
+              } else {
+                throw new Error('platformName not specified');
+              }
+              return scrollToFindElement(attempts + 1);
             } else {
               self.emit('complete');
             }
@@ -29,8 +48,20 @@ ScrollDownToElement.prototype.command = function (xStart, yStart, xEnd, yEnd, el
       self.fail();
     }
   };
+  scrollToFindElement(0);
+};
 
-  scrollToFindElement(maxAttempts, 0);
+ScrollDownToElement.prototype.fail = function (actual, expected) {
+  const pactual = actual || "not visible";
+  const pexpected = expected || "visible";
+  const message = this.failureMessage;
+
+  this.client.assertion(false, pactual, pexpected, util.format(message), true);
+
+  if (this.cb) {
+    this.cb.apply(this.client.api, []);
+  }
+  this.emit("complete");
 };
 
 module.exports = ScrollDownToElement;
