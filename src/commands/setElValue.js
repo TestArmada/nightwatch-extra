8562f1ab0aec9d1a040077a1f86d788f81f4bf0c
cahrens <cahrens@walmartlabs.com>
2020-03-17 09:55:12 -0700

    added hack for ios

--- src/commands/setElValue.js
+++ src/commands/setElValue.js
@@ -1,41 +1,98 @@
-import util from "util";
+import util from 'util';
 
-import selectorUtil from "../util/selector";
-import ClickEl from "./clickEl";
+import selectorUtil from '../util/selector';
+import ClickEl from './clickEl';
 
-const SetElValue = function (nightwatch = null, customizedSettings = null) {
+const SetElValue = function(nightwatch = null, customizedSettings = null) {
   ClickEl.call(this, nightwatch, customizedSettings);
-  this.cmd = "setelvalue";
+  this.cmd = 'setelvalue';
 };
 
 util.inherits(SetElValue, ClickEl);
 
-SetElValue.prototype.do = function (magellanSel) {
+SetElValue.prototype.do = function(magellanSel) {
   const self = this;
-  const now = (new Date()).getTime();
+  const now = new Date().getTime();
   this.time.executeAsyncTime = now - self.startTime;
-  this.client.api
-    .setValue(
-    "css selector",
-    `[${this.selectorPrefix}='${magellanSel}']`,
-    this.valueToSet,
-    () => {
-      self.time.seleniumCallTime = (new Date()).getTime() - now;
-      self.pass({});
-    });
+  if (this.client.api.capabilities.platformName === 'iOS') {
+    this.client.api.execute(
+      function(selector, value) {
+        var elem = document.querySelector(selector);
+        if (elem) {
+          elem.scrollIntoView();
+          if (elem.nodeName === 'SELECT') {
+            var option;
+            for (var i = 0; i < elem.options.length; i++) {
+              if (
+                elem.options[i].text === value ||
+                elem.options[i].value === value
+              ) {
+                option = elem.options[i].value;
+                break;
+              }
+            }
+            if (!option) {
+              throw new Error(`Option [${option}] not found in select options`);
+            }
+            var nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+              window.HTMLSelectElement.prototype,
+              'value'
+            ).set;
+            nativeInputValueSetter.call(elem, option);
+            var inputEvent = new Event('change', { bubbles: true });
+            elem.dispatchEvent(inputEvent);
+          } else {
+            var nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+              window.HTMLInputElement.prototype,
+              'value'
+            ).set;
+            nativeInputValueSetter.call(elem, value);
+            var inputEvent = new Event('input', { bubbles: true });
+            elem.dispatchEvent(inputEvent);
+          }
+        } else {
+          throw new Error(`Element [${selector}] not found!`);
+        }
+      },
+      [`[${this.selectorPrefix}='${magellanSel}']`, this.valueToSet],
+      function(result) {
+        self.time.seleniumCallTime = new Date().getTime() - now;
+        if (result.status === -1) {
+          //fail
+          self.failureMessage =
+            self.failureMessage + `. Reason ${result.value.message}`;
+          self.fail({});
+        } else {
+          self.pass({});
+        }
+      }
+    );
+  } else {
+    this.client.api.setValue(
+      'css selector',
+      `[${this.selectorPrefix}='${magellanSel}']`,
+      this.valueToSet,
+      () => {
+        self.time.seleniumCallTime = new Date().getTime() - now;
+        self.pass({});
+      }
+    );
+  }
 };
 
-SetElValue.prototype.command = function (selector, valueToSet, cb) {
+SetElValue.prototype.command = function(selector, valueToSet, cb) {
   this.selector = selectorUtil.normalize(selector);
   this.valueToSet = valueToSet;
   this.cb = cb;
 
-  this.successMessage = `Selector <${this.selector}> set value to`
-    + ` [${this.valueToSet}] after %d milliseconds`;
-  this.failureMessage = `Selector <${this.selector}> could not set`
-    + ` value to [${this.valueToSet}] after %d milliseconds`;
+  this.successMessage =
+    `Selector <${this.selector}> set value to` +
+    ` [${this.valueToSet}] after %d milliseconds`;
+  this.failureMessage =
+    `Selector <${this.selector}> could not set` +
+    ` value to [${this.valueToSet}] after %d milliseconds`;
 
-  this.startTime = (new Date()).getTime();
+  this.startTime = new Date().getTime();
 
   // Track how many times we've seen selector as :visible
   this.seenCount = 0;
